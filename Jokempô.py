78858a773901c6a8e504f85ca094fab296aa0ac4
Ana Flávia Pizati <111707312+anaflaviapizati@users.noreply.github.com>
2023-04-01 13:13:28 -0300

    Criei Jokempô.py
    Código do jogo em python adicionado

---
+++ Jokempô.py
@@ -0,0 +1,54 @@
+from random import randint
+
+itens = ('Pedra', 'Papel', 'Tesoura')
+tony_jogada = randint(0, 2)
+print('''Escolha: 
+[0] Pedra
+[1] Papel
+[2] Tesoura''')
+jogador = int(input(''))
+print('''
+jo
+ken
+pô!!''')
+print('-='*11)
+print('Jogador jogou: {}'.format(itens[jogador]))
+print('Tony jogou: {}'.format(itens[tony_jogada]))
+print('-='*11)
+if tony_jogada == 0:
+
+    if jogador == 0:
+        print('EMPATE!')
+
+    elif jogador == 1:
+        print('VOCÊ GANHOU!')
+
+    elif jogador == 2:
+        print('COMPUTADOR GANHOU!')
+
+elif tony_jogada == 1:
+
+    if jogador == 0:
+
+        print('COMPUTADOR GANHOU!')
+
+    elif jogador == 1:
+
+        print('EMPATE')
+
+    elif jogador == 2:
+
+        print('VOCÊ GANHOU!')
+
+elif tony_jogada == 2:
+
+    if jogador == 0:
+        print('VOCÊ GANHOU!')
+
+    elif jogador == 1:
+        print('COMPUTADOR GANHOU!')
+
+    elif jogador == 2:
+        print('EMPATE')
+
+input('Aperte enter para sair')
